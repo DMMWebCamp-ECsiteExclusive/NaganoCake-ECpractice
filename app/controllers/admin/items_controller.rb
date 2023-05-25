2948388bf0d6f4a6a7602a06be3e2cc870a20317
ttm-mk <129599412+ttm-mk@users.noreply.github.com>
2023-05-25 21:57:53 +0000

    [Break]商品新規登録機能

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -7,7 +7,7 @@ class Admin::ItemsController < ApplicationController
   def create
     item = Item.new(item_params)
     item.save
-    redirect_to admin_items_show_path
+    redirect_to admin_item_path
   end
 
   def index
@@ -25,7 +25,7 @@ class Admin::ItemsController < ApplicationController
   private
   
   def item_params
-    params.require(:item).permit(:name, :introduction, :price, :image)
+    params.require(:item).permit(:name, :introduction, :price, :item_image, :genre_id, :is_sale)
   end
   
 end
