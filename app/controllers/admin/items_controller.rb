0977be25f29bda08f14c66376acdf8976f1890d2
ttm-mk <129599412+ttm-mk@users.noreply.github.com>
2023-05-26 02:39:06 +0000

    [Update]管理者商品各画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -11,7 +11,8 @@ class Admin::ItemsController < ApplicationController
   end
 
   def index
-    @items = Item.all
+    @items = Item.page(params[:page])
+    @item = Item.find(params[:id])
   end
 
   def show
