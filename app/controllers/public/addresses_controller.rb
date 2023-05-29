e49ad2497ae9003aa76bcbca60758172b36ba22e
ttm-mk <129599412+ttm-mk@users.noreply.github.com>
2023-05-29 14:41:37 +0000

    [update]配送先編集画面

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,11 +1,11 @@
 class Public::AddressesController < ApplicationController
-  
+
   def index
     @address = Address.new
     @customer = current_customer
     @addresses = Address.all
   end
-  
+
   def create
     address = Address.new(params_address)
     address.save
@@ -15,12 +15,18 @@ class Public::AddressesController < ApplicationController
   def edit
     @address = Address.find(params[:id])
   end
-  
-  
+
+  def update
+    address = Address.find(params[:id])
+    address.update(params_address)
+    redirect_to addresses_path(current_cutomer)
+  end
+
+
   private
-  
+
   def params_address
     params.require(:address).permit(:name, :post_code, :address)
   end
-  
+
 end
