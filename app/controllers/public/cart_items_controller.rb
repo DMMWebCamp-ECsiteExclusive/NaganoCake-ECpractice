323b27d16345ddffff490cd4f727fd8e675313dc
ttm-mk <129599412+ttm-mk@users.noreply.github.com>
2023-06-01 06:47:10 +0000

    [Break]カート一覧画面

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -3,13 +3,13 @@ class Public::CartItemsController < ApplicationController
   def index
     @cart_items = CartItem.all
     @item = Item.find(params[:id])
+    @cart_item = CartItem
     @price = @item.price*1.1
-    @customer = current_customer
   end
 
   def create
     cart_item = CartItem.new(cart_item_params)
-    @customer.id = current_customer.id
+    cart_item.customer_id = current_customer.id
     cart_item.save
     redirect_to cart_items_path
   end
