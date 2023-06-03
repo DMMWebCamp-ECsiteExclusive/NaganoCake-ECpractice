64c22a84b5265fdc87947395678b975b9ec51087
ttm-mk <129599412+ttm-mk@users.noreply.github.com>
2023-06-03 14:13:09 +0000

    [Braek]カート編集機能

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,24 +1,18 @@
 class Public::CartItemsController < ApplicationController
 
   def index
-    @cart_items = CartItem.all
-    @item = CartItem.find(cart_item_params[:item_id])
-    @cart_item = CartItem
+    # @cart_items = CartItem.all
+    # @cart_item = CartItem.find(cart_item_params[:item_id])
+    @cart_items = current_customer.cart_items.all
+    @total_price = @cart_items.inject(0) { |sum, cart_item| sum + cart_item.subtotal }
   end
-  
-  def with_tax_price
-    (price*1.1).ceil
-  end
-  
-  def subtotal
-    item.with_tax_price * amount
-  end
-    
+
 
   def create
     cart_item = CartItem.new(cart_item_params)
 
     if customer_signed_in?
+      cart_item.customer_id = current_customer.id
       cart_item.save
       redirect_to cart_items_path
     else
@@ -27,19 +21,21 @@ class Public::CartItemsController < ApplicationController
   end
 
   def update
-    cart_item = CartItem.find(cart_item_params[:item_id])
+    cart_item = CartItem.find(cart_item_params[:amount])
+    # cart_item.customer_id = current_customer.id
     cart_item.update(cart_item_params)
     redirect_to cart_items_path
   end
 
   def destroy
     cart_item = CartItem.find(cart_item_params[:item_id])
+    cart_item.customer_id = current_customer.id
     cart_item.destroy
     redirect_to cart_items_path
   end
 
   def destroy_all
-    cart_items = CartItem.item.all
+    cart_item.customer_id = current_customer.id
     current_customer.cart_items.destroy_all
     redirect_to cart_items_path
   end
--- app/models/item.rb
+++ app/models/item.rb
@@ -16,5 +16,10 @@ class Item < ApplicationRecord
     end
     item_image.variant(resize_to_limit: [width, height]).processed
   end
+  
+  def with_tax_price
+    (price * 1.1).ceil
+  end
+
 
 end
