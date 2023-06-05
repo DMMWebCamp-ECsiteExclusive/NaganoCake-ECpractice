269ac781f106a6be82d08966f810781ecb1066cd
ttm-mk <129599412+ttm-mk@users.noreply.github.com>
2023-06-05 15:32:54 +0000

    [Break]注文情報画面

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,9 @@
 class Admin::OrdersController < ApplicationController
+  
   def show
+    @orders = Order.page(params[:page])
+    @name = (last_name + first_name)
+    @amounts = order_detail.amount.all
   end
+  
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,6 @@ class Order < ApplicationRecord
   has_many :order_details, dependent: :destroy
   
   enum payment_method: { credit_card: 0, transfer: 1 }
+  
+  enum status: { waiting_payment: 0, deposited: 1, production: 2, preparing: 3, shipped: 4 
 end
