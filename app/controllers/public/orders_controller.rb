e6e39508a5a2cff9dfede56a51639987d828005a
ttm-mk <129599412+ttm-mk@users.noreply.github.com>
2023-06-06 06:42:37 +0000

    [Break]カート確認画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -10,24 +10,23 @@ class Public::OrdersController < ApplicationController
   end
   
   def confirm
-    byebug
-    # @order = Order.new(order_params)
-    # if params[:order][:address_id] == "0"
-    #   @order = current_customer
-    #   @order_name = current_customer.last_name + current_customer.first_name
+    @order = Order.new(order_params)
+    if params[:order][:address_id] == "0"
+      @order = current_customer
+      @order_name = current_customer.last_name + current_customer.first_name
       
-    # elsif params[:order][:address_id] == "1"
-    #   @order = Address.find(params[:id])
-    #   @order_name = @order.name
+    elsif params[:order][:address_id] == "1"
+      @order.post_code = Order.find(params[:order][:post_code])
+      @order_name = Order.find([:order][:name])
       
-    # else params[:order][:address_id] == "2"
-    #   @order = Address.find(params[:id])
-    #   @order_name = @order.name
-      
-    # end
-    
-    # @order.save
-    # redirect_to orders_thanks_path
+    else params[:order][:address_id] == "2"
+      @order = Order.find(params[:id])
+      @order_name = @order.name
+
+    end
+  end
+  
+  def create
   end
 
   def thanks
@@ -43,6 +42,6 @@ class Public::OrdersController < ApplicationController
   private
   
   def order_params
-    params.require(:order).permit(:payment_method, :post_code, :address, :name)
+    params.require(:order).permit(:payment_method, :post_code, :address, :name, :address_id, :new_post_code, :new_address, :new_name)
   end
 end
