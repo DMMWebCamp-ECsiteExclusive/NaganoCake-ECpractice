ce84e632ba0f12bf9a72dadfe8f2443da19f7b4d
ttm-mk <129599412+ttm-mk@users.noreply.github.com>
2023-06-13 05:05:30 +0000

    [Update]top修正

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,6 +2,8 @@ class Public::HomesController < ApplicationController
   before_action :authenticate_customer!, except: [:top, :about]
 
   def top
+    @items = Item.order('id DESC').limit(4)
+    # @item = Item.find(params[:id])
   end
 
   def about
